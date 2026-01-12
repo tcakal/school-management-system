602ec9636a031a28267f04ef1eda4f6d075a2e78
Tarık Çakal <tec@Tarks-MacBook-Pro.local>
2026-01-12 17:55:14 +0300

    Add version log for deployment verification

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Version Check
+console.log('App Version: 2026-01-12 v3 (UI Scaling & Avatars)');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
